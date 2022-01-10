522799ba98a8946d67ec88427525dabd0a37a0a5
Mohamed Nizar El Ouahmani <mednizar.elouahmani@gmail.com>
2022-01-10 10:21:52 +0100

    first-commit

---
+++ main.js
@@ -0,0 +1,116 @@
+var session_seconds = "00";
+var session_minutes = 25;
+// Audio file
+var bell = new Audio("bell.mp3");
+
+// Starting template for the timer
+function template() {
+  document.getElementById("minutes").innerHTML = session_minutes;
+  document.getElementById("seconds").innerHTML = session_seconds;
+}
+
+function start_timer() {
+  console.log("Test");
+  // Change the minutes and seconds to starting time
+  session_minutes = 24;
+  session_seconds = 59;
+
+  // Add the seconds and minutes to the page
+  document.getElementById("minutes").innerHTML = session_minutes;
+  document.getElementById("seconds").innerHTML = session_seconds;
+
+  // Start the countdown
+  let minutes_interval = setInterval(minutesTimer, 60000);
+  let seconds_interval = setInterval(secondsTimer, 1000);
+
+  // Function for minute counter
+  function minutesTimer() {
+    session_minutes = session_minutes - 1;
+    document.getElementById("minutes").innerHTML = session_minutes;
+  }
+
+  // Function for second counter
+  function secondsTimer() {
+    session_seconds = session_seconds - 1;
+    document.getElementById("seconds").innerHTML = session_seconds;
+    // If reached 0 then end the session
+    if (session_seconds <= 0) {
+      if (session_minutes <= 0) {
+        clearInterval(minutes_interval);
+        clearInterval(seconds_interval);
+
+        // Add the message to the html
+        document.getElementById("done").innerHTML =
+          "Good Job ! Now Take a Break";
+
+        // Make the html message div visible
+        document.getElementById("done").classList.add("show_message");
+        bell.play();
+      }
+      session_seconds = 60;
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const list_el = document.querySelector("#tasks");
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		const task = input.value;
+
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
+
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
+
+		task_content_el.appendChild(task_input_el);
+
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
+
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) => {
+			if (task_edit_el.innerText.toLowerCase() == "edit") {
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else {
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
+});
